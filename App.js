91e10264e3787497957dd96c354169e521d997c1
Davin Wijaya <wijayadavin@gmail.com>
2020-12-20 15:09:07 +0700

    init

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { App } from './app/index';
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+
 
 const Root = () => {
   return (
-    < Provider store={store} >
-      <App />
+    <Provider store={store}>
+      <StatusBar />
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
     </Provider >
   )
 }
+
 
 export default Root
--- app/index.js
+++ app/index.js
@@ -1,13 +1,46 @@
+// import React from 'react'
+// import { View } from 'react-native'
+// import { Navigation } from './utils/router'
+
+
+// const App = ({ }) => (
+//     < View style={{ flex: 1, flexDirection: 'column' }}>
+//         <Navigation />
+//     </View >
+// )
+
 import React from 'react'
-import { View } from 'react-native'
-import { Navigation } from './utils/router'
+import { connect } from 'react-redux'
 
+//Navigator
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
-const App = ({ }) => (
-    < View style={{ flex: 1, flexDirection: 'column' }}>
-        <Navigation />
-    </View >
-)
+//Screens
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import MainNavigator from './MainNavigator'
 
+function App(props) {
+    return (
+        <Stack.Navigator headerMode='none'>
+            { props.verify ? <Stack.Screen name='Home' component={MainNavigator} />
+                : (
+                    <>
+                        <Stack.Screen name='Login' component={LoginScreen} />
+                        <Stack.Screen name='Register' component={RegisterScreen} />
+                    </>
+                )}
+        </Stack.Navigator>
+    )
+}
 
-export { App }
+const reduxState = (state) => ({
+    verify: state.login.isLoggedIn,
+})
+
+const reduxDispatch = (dispatch) => ({
+
+})
+
+export default connect(reduxState, reduxDispatch)(App);
